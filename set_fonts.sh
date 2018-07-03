65e01bef3be42e014f1b73b308f43d9a5c8417f5
csegura <romheat@gmail.com>
2018-07-03 21:09:15 +0200

    fonts

---
+++ set_fonts.sh
@@ -0,0 +1,9 @@
+brew tap homebrew/cask-fonts 
+brew cask install font-hack
+brew cask install font-iconsolata
+brew cask install font-envy-code-r
+brew cask install font-proggyclean-nerd-font
+brew cask install font-proggyclean-nerd-font-mono
+
+
+
