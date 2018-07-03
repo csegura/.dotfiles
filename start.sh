94f988572a979202c51064d9b207dbdcb3ce6ecb
csegura <romheat@gmail.com>
2018-07-03 21:44:21 +0200

    VSCode

--- start.sh
+++ start.sh
@@ -41,7 +41,7 @@ ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # iterm2
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iTerm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
