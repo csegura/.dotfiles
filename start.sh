d78bf6045513d3ec59d1174bebe3b309a6064085
csegura <romheat@gmail.com>
2018-07-03 21:40:37 +0200

    VSCode

--- start.sh
+++ start.sh
@@ -27,6 +27,12 @@ ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
+# link VSCode
+ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User/snippets
+
+
 # link textmate
 # ln -s ~/.dotflles/textmate/tm_properties ~/.tm_properties
 
