45174bdf9ae921f4514f4a843f153880588fe6c3
csegura <romheat@gmail.com>
2019-03-31 23:00:35 +0200

    changes

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,3 +2,7 @@
 HISTSIZE=10000
 SAVEHIST=9000
 HISTFILE=~/.zsh_history
+setopt    appendhistory     #Append history to the history file (no overwriting)
+setopt    sharehistory      #Share history across terminals
+setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+
