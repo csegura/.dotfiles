94be5155a9d88ee652b6ae5bfb81e922a7facfc9
csegura <romheat@gmail.com>
2022-08-15 10:19:53 +0200

    use pwd

--- start_docker.sh
+++ start_docker.sh
@@ -2,36 +2,39 @@
 git submodule init
 git submodule update
 
+path = $(pwd)
+echo "path: $path"
+
 # link zsh 
-ln -sf $1/.dotfiles/zsh ~/.zsh
-ln -sf $1/.dotfiles/zsh/zshrc ~/.zshrc
-ln -sf $1/.dotfiles/zsh/zshenv ~/.zshenv
-ln -sf $1/.dotfiles/zsh/zprofile ~/.zprofile
+ln -sf $path/.dotfiles/zsh ~/.zsh
+ln -sf $path/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf $path/.dotfiles/zsh/zshenv ~/.zshenv
+ln -sf $path/.dotfiles/zsh/zprofile ~/.zprofile
 
 # link ssh 
-ln -sf $1/.dotfiles/ssh/config ~/.ssh/config
+ln -sf $path/.dotfiles/ssh/config ~/.ssh/config
 
 # link vim 
-ln -sf $1/.dotfiles/vim ~/.vim
-ln -sf $1/.dotfiles/vim/vimrc ~/.vimrc
-ln -sf $1/.dotfiles/vim/gvimrc ~/.gvimrc
+ln -sf $path/.dotfiles/vim ~/.vim
+ln -sf $path/.dotfiles/vim/vimrc ~/.vimrc
+ln -sf $path/.dotfiles/vim/gvimrc ~/.gvimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
 # inside vim :BundleInstall
 
 # link bash
-ln -sf $1/.dotfiles/bash/bash_profile ~/.bash_profile
-ln -sf $1/.dotfiles/bash/bashrc ~/.bashrc
-ln -sf $1/.dotfiles/bash/bash_aliases ~/.bash_aliases
-ln -sf $1/.dotfiles/bash/bash_history ~/.bash_history
+ln -sf $path/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf $path/.dotfiles/bash/bashrc ~/.bashrc
+ln -sf $path/.dotfiles/bash/bash_aliases ~/.bash_aliases
+ln -sf $path/.dotfiles/bash/bash_history ~/.bash_history
 
 # link git
-ln -sf $1/.dotfiles/git/gitconfig ~/.gitconfig
-ln -sf $1/.dotfiles/git/gitignore_global ~/.gitignore_global
+ln -sf $path/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf $path/.dotfiles/git/gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # link tmux
-ln -sf $1/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf $path/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 
