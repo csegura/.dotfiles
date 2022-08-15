8cc08646567cd21ce298ad09acd4ef7d2e68e88c
csegura <romheat@gmail.com>
2022-08-15 10:30:59 +0200

    fix path

--- start_docker.sh
+++ start_docker.sh
@@ -7,35 +7,35 @@ SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 echo "path: $SCRIPT_DIR"
 
 # link zsh 
-ln -sf $SCRIPT_DIR/.dotfiles/zsh ~/.zsh
-ln -sf $SCRIPT_DIR/.dotfiles/zsh/zshrc ~/.zshrc
-ln -sf $SCRIPT_DIR/.dotfiles/zsh/zshenv ~/.zshenv
-ln -sf $SCRIPT_DIR/.dotfiles/zsh/zprofile ~/.zprofile
+ln -sf $SCRIPT_DIR/zsh ~/.zsh
+ln -sf $SCRIPT_DIR/zsh/zshrc ~/.zshrc
+ln -sf $SCRIPT_DIR/zsh/zshenv ~/.zshenv
+ln -sf $SCRIPT_DIR/zsh/zprofile ~/.zprofile
 
 # link ssh 
-ln -sf $SCRIPT_DIR/.dotfiles/ssh/config ~/.ssh/config
+ln -sf $SCRIPT_DIR/ssh/config ~/.ssh/config
 
 # link vim 
-ln -sf $SCRIPT_DIR/.dotfiles/vim ~/.vim
-ln -sf $SCRIPT_DIR/.dotfiles/vim/vimrc ~/.vimrc
-ln -sf $SCRIPT_DIR/.dotfiles/vim/gvimrc ~/.gvimrc
+ln -sf $SCRIPT_DIR/vim ~/.vim
+ln -sf $SCRIPT_DIR/vim/vimrc ~/.vimrc
+ln -sf $SCRIPT_DIR/vim/gvimrc ~/.gvimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
 # inside vim :BundleInstall
 
 # link bash
-ln -sf $SCRIPT_DIR/.dotfiles/bash/bash_profile ~/.bash_profile
-ln -sf $SCRIPT_DIR/.dotfiles/bash/bashrc ~/.bashrc
-ln -sf $SCRIPT_DIR/.dotfiles/bash/bash_aliases ~/.bash_aliases
-ln -sf $SCRIPT_DIR/.dotfiles/bash/bash_history ~/.bash_history
+ln -sf $SCRIPT_DIR/bash/bash_profile ~/.bash_profile
+ln -sf $SCRIPT_DIR/bash/bashrc ~/.bashrc
+ln -sf $SCRIPT_DIR/bash/bash_aliases ~/.bash_aliases
+ln -sf $SCRIPT_DIR/bash/bash_history ~/.bash_history
 
 # link git
-ln -sf $SCRIPT_DIR/.dotfiles/git/gitconfig ~/.gitconfig
-ln -sf $SCRIPT_DIR/.dotfiles/git/gitignore_global ~/.gitignore_global
+ln -sf $SCRIPT_DIR/git/gitconfig ~/.gitconfig
+ln -sf $SCRIPT_DIR/git/gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # link tmux
-ln -sf $SCRIPT_DIR/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
 
 
